f6c9a182a69d4bf33b92fc93cc1e160beffe9a33
luisemiliotisocco <luisemiliotisocco@gmail.com>
2021-12-07 16:33:47 -0300

    ventas por manzana

--- src/01_Carga-de-datos.R
+++ src/01_Carga-de-datos.R
@@ -1,12 +1,19 @@
 library(tidyverse)
 library(sf)
 library(ggplot2)
+library(lubridate)
+options(scipen=999)
 
 proj <- "+proj=laea +lat_0=-40 +lon_0=-60 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs"
 
 barrios <- st_read("data/raw/GCABA/barrios/barrios_badata_wgs84.shp") %>% 
     st_transform(proj) %>% 
     select(BARRIO)
+
+manzanas <- st_read("data/raw/GCABA/manzanas/manzanas.geojson") %>% 
+    st_transform(proj) %>% 
+    select(SM, MZ_SUP) %>% 
+    st_join(barrios)
 
 #_______________________________________________________________________________
 ### Filtramos el df de properati, que pesa 1GB
@@ -28,10 +35,41 @@ prop <- vroom::vroom("data/raw/Properati/ar_properties_filtrado.csv") %>%
     rename(id=...1) %>% 
     st_as_sf(coords = c("lon", "lat"), crs = 4326) %>% 
     st_transform(proj) %>% 
-    st_intersection(barrios)
+    st_intersection(manzanas) %>% 
+    mutate(price_m2=price/surface_total)
 
 # Precio promedio por barrio
 prop_precio_barrio <- prop %>% 
     as.data.frame() %>% 
     group_by(BARRIO) %>% 
     summarise(promedio_barrio=mean(price))
+
+# Manzanas con más ventas
+venta_por_manzana <- prop %>% 
+    as.data.frame() %>% 
+    group_by(SM) %>% 
+    summarise(cantidad_venta=n())
+
+manzanas <- manzanas %>% left_join(venta_por_manzana, by="SM")
+
+ggplot()+
+    geom_sf(data=manzanas, fill="grey90", color="grey80")+
+    geom_sf(data=manzanas %>% dplyr::filter(!is.na(cantidad_venta)), aes(fill=cantidad_venta), color=NA)+
+    scale_fill_viridis_c(option = "magma", direction=-1) +
+    theme_void()
+
+##
+prop <- prop %>% left_join(venta_por_manzana, by="SM")
+
+
+# FECHAS
+
+
+prop <- prop %>% 
+    mutate(fecha=as_date(created_on))
+
+manzana_con_mas_ventas <- prop %>% dplyr:: filter(SM=="098-005M")
+
+ggplot(manzana_con_mas_ventas) + 
+    geom_bar(aes(x = fecha), fill="orange", alpha=.5, color="black")
+
